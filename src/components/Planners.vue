48bb0599e78c650a4a7f147f61d075369f55355a
151250206 <151250206@smail.nju.edu.cn>
2017-11-19 13:29:07 +0800

    <dev> : <update Experience and Planners>

--- src/components/Planners.vue
+++ src/components/Planners.vue
@@ -1,48 +1,47 @@
 <template>
   <div>
     <div class="planners-wrapper">
-      <h1>WE are<span>  Planners</span></h1>
+      <h1>{{$t('planners.title_first')}}<span>{{$t('planners.title_second')}}</span></h1>
 
       <el-row :gutter="20">
         <el-col :span="8">
           <div>
-            <p>Urban Planning is a S.Y. flagship consultancy service. We offer a full range of urban planning and design services across all aspects of the development process. Our primary aim is to understand and execute the vision and objectives of our clients’, and we back this up by fully applying our significant resources to each project in order to achieve the best outcomes.
-            </p>
+            <p>{{$t('planners.p1')}}</p>
           </div>
         </el-col>
         <el-col :span="8">
-          <p>multidisciplinary team of experienced professional urban planners, urban designers, and infrastructure engineers with an in-depth and robust appreciation of urban planning systems and process in Africa, and specifically Africa.</p>
+          <p>{{$t('planners.p2')}}</p>
         </el-col>
         <el-col :span="8">
-          <p>S.Y. take an holistic view to analysing the potential consequences of each development we undertake from social and environmental perspectives in order to generate creative, financially viable and, most importantly sustainable schemes.</p>
+          <p>{{$t('planners.p3')}}</p>
 
         </el-col>
       </el-row>
     </div>
     <el-row :gutter="20">
       <el-col :span="12">
-        <h2><span>Master  Plans</span></h2>
+        <h2><span>{{$t('planners.p4')}}</span></h2>
         <ul style="width: 60%; margin-left: 20%; text-align: left">
-          <li style="font-size: 20px;">S.Y. help cut through complex local requirements to assist our clients in introducing new ideas, approaches, products, and technologies, while providing the leadership and management support necessary to ensure that international standards are being followed.</li>
-          <li style="font-size: 20px;">A full-scale Master Plan requires an accurate inventory of the population and study of local land use patterns, economic and demographic predictions as well as a thorough understanding of environmental issues and anticipation of infrastructure changes that may occur.</li>
+          <li style="font-size: 20px;">{{$t('planners.p5')}}</li>
+          <li style="font-size: 20px;">{{$t('planners.p6')}}</li>
         </ul>
       </el-col>
       <el-col :span="12">
-        <h3 style="width: 80%; white-space: normal; text-align: left; margin-left: 10%"><span>The development of Master Plans is necessary to fulfil the following functions; </span></h3>
+        <h3 style="width: 80%; white-space: normal; text-align: left; margin-left: 10%"><span>{{$t('planners.p7')}}</span></h3>
         <ul style="width: 80%; margin-left: 10%; text-align: left">
-          <li>Establish development potential of land</li>
-          <li>Fulfil local and social requirements</li>
-          <li>Safeguard the real estate value</li>
-          <li>Enhance the value of the real estate</li>
+          <li>{{$t('planners.p8')}}</li>
+          <li>{{$t('planners.p9')}}</li>
+          <li>{{$t('planners.p10')}}</li>
+          <li>{{$t('planners.p11')}}</li>
         </ul>
-        <h3 style="width: 80%; white-space: normal; text-align: left; margin-left: 10%"><span>The Master Plan may be used for any one of the following purposes; </span></h3>
+        <h3 style="width: 80%; white-space: normal; text-align: left; margin-left: 10%"><span>{{$t('planners.p12')}}</span></h3>
         <ul style="width: 80%; margin-left: 10%; text-align: left">
-          <li>Investment Licenses</li>
-          <li>Project Finance</li>
-          <li>Land Use Right Certificate</li>
-          <li>Facilitate Divestment</li>
-          <li>Construction Permit</li>
-          <li>Development Control</li>
+          <li>{{$t('planners.p13')}}</li>
+          <li>{{$t('planners.p14')}}</li>
+          <li>{{$t('planners.p15')}}</li>
+          <li>{{$t('planners.p16')}}</li>
+          <li>{{$t('planners.p17')}}</li>
+          <li>{{$t('planners.p18')}}</li>
         </ul>
       </el-col>
     </el-row>
