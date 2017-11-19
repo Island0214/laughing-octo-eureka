7c043c43a1c1f2a03d08bbe6f70d00a21b9f0b97
Island0214 <549378813@qq.com>
2017-11-19 12:11:33 +0800

    <feat> : <完成planner页面>

---
+++ src/page/PlannersPage.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+    <Header/>
+    <NavBar/>
+    <Planners/>
+    <Footer/>
+  </div>
+
+</template>
+
+<script>
+  import Header from '../components/Header.vue'
+  import NavBar from '../components/NavBar.vue'
+  import Planners from '../components/Planners.vue'
+  import Footer from '../components/Footer.vue'
+  export default {
+    name: 'PlannersPage',
+    components: {
+      Header,
+      NavBar,
+      Planners,
+      Footer
+    }
+  }
+</script>
